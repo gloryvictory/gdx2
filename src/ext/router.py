44a4652351a58c18519ada0fe106170323b5336f
glory <zamaraev@gmail.com>
2023-06-27 00:03:07 +0500

    need to see session

--- src/ext/router.py
+++ src/ext/router.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, UploadFile  # , Depends, HTTPException
+from fastapi import APIRouter, UploadFile, Depends  # , Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.ext.services import ext_get_all_count, ext_upload_file
+from src.database import get_async_session
 
 router_ext = APIRouter(
     # prefix="/files",
@@ -25,37 +28,3 @@ async def get_count():
 async def upload_file(file: UploadFile):
     content = await ext_upload_file(file)
     return content
-#
-# @router_files.post(path='/source/',
-#                    status_code=200,
-#                    name='Добавить источник',
-#                    tags=['Расширения'],
-#                    description='Добавить источник', )
-# async def source_add(folder: str,  session: AsyncSession = Depends(get_async_session)):
-#     content = await src_add(folder, session)
-#     return content
-#
-# @router_files.get(path='/source/',
-#                    status_code=200,
-#                    name='Получить все источники',
-#                    tags=['Расширения'],
-#                    description='Получить все источники', )
-# async def source_get_all(session: AsyncSession = Depends(get_async_session)):
-#     content = await src_get_all(session)
-#     return content
-#
-# @router_files.get(path='/source/{id}',
-#                    status_code=200,
-#                    name='Получить все источник',
-#                    tags=['Расширения'],
-#                    description='Получить все источник', )
-# async def source_get_all(id:int, session: AsyncSession = Depends(get_async_session)):
-#     content = await src_get_by_id(id, session)
-#     return content
-
-# async def add_specific_operations(src_new: FILE_SRC_S_CREATE, session: AsyncSession = Depends(get_async_session)):
-#     stmt = insert(FILE_SRC_M).values(**src_new.dict())
-#     print(stmt)
-#     await session.execute(stmt)
-#     await session.commit()
-#     return {"status": "success"}
--- src/ext/services.py
+++ src/ext/services.py
@@ -1,11 +1,11 @@
 import os
-
-import openpyxl
+from fastapi import UploadFile, File
+from openpyxl import load_workbook
+from sqlalchemy import insert
 
 from src import cfg
 from src.database import get_async_session
 from src.models import EXT_M
-from fastapi import UploadFile, File
 
 
 async def ext_get_all_count():
@@ -27,13 +27,13 @@ async def ext_get_all_count():
     return content
 
 
-async def ext_upload_file(file: UploadFile = File(...)):
+async def ext_upload_file(file: UploadFile = File(...) ):
     content = {"msg": f"Unknown error"}
     try:
 
         if not os.path.isdir(cfg.FOLDER_UPLOAD):
             os.mkdir(cfg.FOLDER_UPLOAD)
-        file_in = file.filename
+        # file_in = file.filename
         file_out = file.filename.replace(" ", "-")
 
         file_name = os.path.join(os.getcwd(), cfg.FOLDER_UPLOAD, file_out)
@@ -49,7 +49,7 @@ async def ext_upload_file(file: UploadFile = File(...)):
         all_count = 1
         content = {"msg": "Success", "count": all_count, "filename": file.filename}
 
-        await excel2db(file_name)
+        await excel2db(file_name, session)
 
         return content
     except Exception as e:
@@ -68,31 +68,71 @@ async def excel2db(file_in: str):
     # await ADOC_M.objects.delete(each=True)
     # await AUTHOR_M.objects.delete(each=True)
 
-    # Define variable to load the wookbook
-    wookbook = openpyxl.load_workbook(file_in)
+    async with get_async_session() as session:
+        # all_count = await session.query(EXT_M).count()
+        # content = {"msg": "Success", "count": all_count}
+        # session.close()
 
-    # Define variable to read the active sheet:
-    worksheet = wookbook.active
+        # Define variable to load the wookbook
+        # wookbook = openpyxl.load_workbook(file_in)
+        wookbook = load_workbook(file_in)
 
-    # Iterate the loop to read the cell values
-    # worksheet.max_row
-    # worksheet.max_column
+        # Define variable to read the active sheet:
+        worksheet = wookbook.active
 
-    cnt = 0
-    try:
+        # Iterate the loop to read the cell values
+        # worksheet.max_row
+        # worksheet.max_column
 
-        for value in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=1, max_col=5, values_only=True):
-            print(f"{cnt}")
-            cnt = cnt + 1
-            str_temp = str(value[0])
-            print(str_temp)
+        cnt = 0
+        try:
 
-            # if value[14]:  # Отчет
-            #     str_tmp1 = str(value[14]).strip().replace("_x001E_", "")
-            #     report_name = str_cleanup(str_tmp1)
+            for value in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=1, max_col=5, values_only=True):
+                # print(f"{cnt}")
+                cnt = cnt + 1
+                str_ext = ''
+                str_cat = ''
+                str_desc = ''
+                str_prod = ''
+                str_is_project = ''
+                is_project = False
 
-            # ).save()
+                if value[0]:  # Ext
+                    str_ext = str(value[0]).strip()
+
+                if value[1]:  # Category
+                    str_cat = str(value[1]).strip()
+
+                if value[2]:  # Description
+                    str_desc = str(value[2]).strip()
+
+                if value[3]:  # Product
+                    str_prod = str(value[3]).strip()
+
+                if value[4]:  # is_project
+                    str_is_project = str(value[4]).strip()
+                    if str_is_project.isdigit():
+                        if int(str_is_project) == 1:
+                            is_project = True
 
 
-    except Exception as e:
-        content = {"msg": "error", "data": f"Exception occurred {str(e)}"}
+                print(f"{str_ext} {str_cat} {str_desc} {str_prod} {str_is_project} ")
+
+                stmt = insert(EXT_M).values(ext=str_ext, category=str_cat, description=str_desc, product=str_prod, is_project=is_project)
+                result = await session.execute(stmt)
+                await session.commit()
+
+                # new_src = FILE_SRC_M(folder_src=folder)
+                # session.add(new_src)
+                # await session.commit()
+
+                # conn.commit()
+                # if value[14]:  # Отчет
+                #     str_tmp1 = str(value[14]).strip().replace("_x001E_", "")
+                #     report_name = str_cleanup(str_tmp1)
+
+                # ).save()
+            session.close()
+
+        except Exception as e:
+            content = {"msg": "error", "data": f"Exception occurred {str(e)}"}
