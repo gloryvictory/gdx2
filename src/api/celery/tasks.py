a8fba95509ecdffcc45af90e5c702819da5b02c2
Glory <zamaraev@gmail.com>
2023-10-23 22:50:56 +0500

    celery task started

--- src/api/celery/tasks.py
+++ src/api/celery/tasks.py
@@ -1,7 +1,12 @@
 from datetime import datetime
+import asyncio
+
+import sqlalchemy
+from sqlalchemy.orm import Session
 
 from celery import Celery
-from sqlalchemy import update
+from sqlalchemy import update, MetaData, NullPool
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from src import cfg
 from src.db.db import async_session_maker
@@ -9,42 +14,128 @@ from src.log import set_logger
 from src.models import M_FILE
 
 celery = Celery('tasks', backend='redis://localhost:6379/0', broker='redis://localhost:6379/0')
-# celery = Celery('tasks', broker=cfg.REDIS_URL)
 
+# celery = Celery('tasks', broker=cfg.REDIS_URL)
+# волго-уральская	6
+# западно-сибирская	49483
+# лено-вилюйская	4
+# лено-тунгусская	9
+# мезенская	28788
+# охотская	6
+# прикаспийская	14
+# притихоокеанская	8
+# тимано-печорская	1136
 
 # запуск celery
 # celery -A src.api.celery.tasks:celery worker --loglevel=INFO --pool=solo
 # запуск flower
 # celery -A src.api.celery.tasks:celery flower
+# Удалить все задачи
+#  celery -A src.api.celery.tasks:celery  purge
+
+# @celery.task(serializer='pickle')
+# async def update_file_by_ngp_str(ngp_str: str, lat: float, lon: float, f_path_md5: str):
+#     try:
+#         async with async_session_maker() as session:
+#             time1 = datetime.now()
+#             log = set_logger(cfg.NGP_FILE_LOG)
+#             log.INFO(f"Обработка: {ngp_str}")
+#
+#             # print(file.f_path)
+#             # f_path_md5 = f_path_md5
+#             stmt = (
+#                 update(M_FILE)
+#                 .where(M_FILE.f_path_md5 == f_path_md5)
+#                 .values(ngp=ngp_str, lat=lat, lon=lon)
+#             )
+#             await session.execute(stmt)
+#             await session.commit()
+#
+#             content = {"msg": "Success"}
+#             time2 = datetime.now()
+#             log.INFO(f"Обработано: {ngp_str}. Total time:  + {str(time2 - time1)}")
+#             return content
+#     except Exception as e:
+#         cont_err = f"fail. can't read or update data from table ({M_FILE.__tablename__})"
+#         content = {"msg": "error", "data": f"Exception occurred {str(e)} . {cont_err}"}
+#         print(content)
+#     finally:
+#         if session is not None:
+#             await session.close()
+#     return content
 
 
-@celery.task(serializer='pickle')
-async def update_file_by_ngp_str(ngp_str: str, lat: float, lon: float, f_path_md5: str):
+#
+# Рабочий вариант без Celery!!!
+# async def update_file_by_ngp_str2(ngp_str: str, lat: float, lon: float, *args, **kwargs):
+#     try:
+#         print(f"Обработано: {ngp_str}.!!!!!!")
+#         async with async_session_maker() as session:
+#             time1 = datetime.now()
+#             ngp_str_new = f"%{ngp_str}%"
+#             stmt = (
+#                 update(M_FILE)
+#                 .where(M_FILE.f_path.ilike(ngp_str_new))
+#                 .values(ngp=ngp_str, lat=lat, lon=lon)
+#             )
+#             await session.execute(stmt)
+#             await session.commit()
+#             time2 = datetime.now()
+#             print(f"Обработано: {ngp_str}. Total time:  + {str(time2 - time1)}")
+#     except Exception as e:
+#         cont_err = f"fail. can't read or update data from table ({M_FILE.__tablename__})"
+#         content = {"msg": "error", "data": f"Exception occurred {str(e)} . {cont_err}"}
+#         print(content)
+#     finally:
+#         if session is not None:
+#             await session.close()
+
+
+# from src.cfg import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER, DB_SCHEMA, CONVENTION
+# DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# print(DATABASE_URL)
+# metadata = MetaData(schema=DB_SCHEMA, naming_convention=CONVENTION)
+# engine = create_async_engine(DATABASE_URL, poolclass=NullPool, echo=True)
+# async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+# database = Database()
+# engine = database.get_db_connection()
+# session = database.get_db_session()
+
+# celery_log = get_task_logger(__name__)
+
+celery.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"]
+)
+
+
+# , lat: float, lon: float, *args, **kwargs
+@celery.task(name="update_file_by_ngp")
+def update_file_by_ngp_str2(ngp_str: str, lat: float, lon: float):
+    # ngp_str: str = "qweqweqweqwe"
+    print(f"Обработано: {ngp_str} {lat} {lon}.!!!!!!")
     try:
-        async with async_session_maker() as session:
+        print(f"Обработано: {ngp_str}.!!!!!!")
+        engine = sqlalchemy.create_engine(cfg.DB_DSN2)
+        # create session and add objects
+        with Session(engine) as session:
             time1 = datetime.now()
-            log = set_logger(cfg.NGP_FILE_LOG)
-            log.INFO(f"Обработка: {ngp_str}")
-
-            # print(file.f_path)
-            # f_path_md5 = f_path_md5
+            ngp_str_new = f"%{ngp_str}%"
             stmt = (
                 update(M_FILE)
-                .where(M_FILE.f_path_md5 == f_path_md5)
+                .where(M_FILE.f_path.ilike(ngp_str_new))
                 .values(ngp=ngp_str, lat=lat, lon=lon)
             )
-            await session.execute(stmt)
-            await session.commit()
-
-            content = {"msg": "Success"}
+            session.execute(stmt)
+            session.commit()
             time2 = datetime.now()
-            log.INFO(f"Обработано: {ngp_str}. Total time:  + {str(time2 - time1)}")
-            return content
+            print(f"Обработано: {ngp_str}. Total time:  + {str(time2 - time1)}")
     except Exception as e:
         cont_err = f"fail. can't read or update data from table ({M_FILE.__tablename__})"
         content = {"msg": "error", "data": f"Exception occurred {str(e)} . {cont_err}"}
         print(content)
     finally:
         if session is not None:
-            await session.close()
-    return content
+            session.close()
--- src/api/update/services.py
+++ src/api/update/services.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from sqlalchemy import insert, text, select, update, func
 
 from src import cfg
-from src.api.celery.tasks import update_file_by_ngp_str
+from src.api.celery.tasks import update_file_by_ngp_str2
 from src.db.db import async_session_maker
 from src.log import set_logger
 from src.models import M_NSI_NGP, M_FILE
@@ -19,8 +19,10 @@ def get_ngp_name(ngp_in: str):
 
 async def update_by_ngp():
     content = {"msg": f"error"}
-    log = set_logger(cfg.NGP_FILE_LOG)
+    # log = set_logger(cfg.NGP_FILE_LOG)
+
     try:
+        time1 = datetime.now()
         async with async_session_maker() as session:
             res = await session.scalars(
                 select(M_NSI_NGP)
@@ -36,8 +38,9 @@ async def update_by_ngp():
                 await update_file_by_ngp(ngp_str, lat, lon)
 
             content = {"msg": "Success", "count": cnt, "data": _all}
-
-            return content
+        time2 = datetime.now()
+        print(f"Обработаны все НГП: Total time:  + {str(time2 - time1)}")
+        return content
     except Exception as e:
         cont_err = f"fail. can't read ngp from table ({M_NSI_NGP.__tablename__})"
         content = {"msg": "error", "data": f"Exception occurred {str(e)} . {cont_err}"}
@@ -50,51 +53,63 @@ async def update_by_ngp():
 
 async def update_file_by_ngp(ngp_str: str, lat: float, lon: float):
     content = {"msg": f"error"}
+
     try:
-        async with async_session_maker() as session:
-            time1 = datetime.now()
-            # log = set_logger(cfg.NGP_FILE_LOG)
-            print(f"Обработка: {ngp_str}")
-            # log.INFO(f"Обработка: {ngp_str}")
-            # через полнотекстовый поиск (но он выдает больше результатов, т.к. для него Частная и Частный одно и то же)
+
+        print(f"Обработка: {ngp_str}")
+        # await update_file_by_ngp_str2(ngp_str, lat, lon)
+
+        # result = update_file_by_ngp_str2.delay(ngp_str, lat, lon)
+
+
+        result = update_file_by_ngp_str2.delay(ngp_str, lat, lon)
+
+        # while not result.ready():
+        #     pass
+        # print(result.get())
+
+        # async with async_session_maker() as session:
+        #     time1 = datetime.now()
+        #     # log = set_logger(cfg.NGP_FILE_LOG)
+        #     print(f"Обработка: {ngp_str}")
+        #     # log.INFO(f"Обработка: {ngp_str}")
+        #     # через полнотекстовый поиск (но он выдает больше результатов, т.к. для него Частная и Частный одно и то же)
+        #     # res = await session.scalars(
+        #     #     select(M_FILE)
+        #     #     .where(M_FILE.file_path_fts.match(ngp_str, postgresql_regconfig='russian'))
+        #     #     .order_by(M_FILE.f_path)
+        #     # )
+        #
+        #     update_file_by_ngp_str2.delay(ngp_str, lat, lon)
+        #
+            # ngp_str_new = f"%{ngp_str}%"
             # res = await session.scalars(
             #     select(M_FILE)
-            #     .where(M_FILE.file_path_fts.match(ngp_str, postgresql_regconfig='russian'))
+            #     .where(M_FILE.f_path.ilike(ngp_str_new) ) # lower(f.f_path) like LOWER('%частная%')
             #     .order_by(M_FILE.f_path)
             # )
-            ngp_str_new = f"%{ngp_str}%"
-            res = await session.scalars(
-                select(M_FILE)
-                .where(M_FILE.f_path.ilike(ngp_str_new) ) # lower(f.f_path) like LOWER('%частная%')
-                .order_by(M_FILE.f_path)
-            )
-            # func.lower(M_FILE.f_path)
-            # .where(M_FILE.file_path_fts.match(ngp_str, postgresql_regconfig='russian'))
+            # # func.lower(M_FILE.f_path)
+            # # .where(M_FILE.file_path_fts.match(ngp_str, postgresql_regconfig='russian'))
+            #
+            # _all = res.all()
+            # cnt = len(_all)
+            # for file in _all:
+            #     f_path_md5 = file.f_path_md5
+            #     update_file_by_ngp_str.delay(ngp_str, lat, lon, f_path_md5)
+            #     # print(file.f_path)
+            #     # f_path_md5 = file.f_path_md5
+            #     # stmt = (
+            #     #     update(M_FILE)
+            #     #     .where(M_FILE.f_path_md5 == f_path_md5)
+            #     #     .values(ngp=ngp_str, lat=lat, lon=lon)
+            #     # )
+            #     # await session.execute(stmt)
+            #     # await session.commit()
 
-            _all = res.all()
-            cnt = len(_all)
-            for file in _all:
-                f_path_md5 = file.f_path_md5
-                update_file_by_ngp_str.delay(ngp_str, lat, lon, f_path_md5)
-                # print(file.f_path)
-                # f_path_md5 = file.f_path_md5
-                # stmt = (
-                #     update(M_FILE)
-                #     .where(M_FILE.f_path_md5 == f_path_md5)
-                #     .values(ngp=ngp_str, lat=lat, lon=lon)
-                # )
-                # await session.execute(stmt)
-                # await session.commit()
-
-            content = {"msg": "Success"}
-            time2 = datetime.now()
-            print(f"Обработано: {ngp_str}. Total time:  + {str(time2 - time1)}")
-            return content
+        content = {"msg": "Success"}
+        return content
     except Exception as e:
         cont_err = f"fail. can't read file from table ({M_NSI_NGP.__tablename__})"
         content = {"msg": "error", "data": f"Exception occurred {str(e)} . {cont_err}"}
         print(content)
-    finally:
-        if session is not None:
-            await session.close()
     return content
