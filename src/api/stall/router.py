eb1aadcaa6e762e9a6f191cd6afc0ba9bf872338
Glory <zamaraev@gmail.com>
2025-03-14 22:29:55 +0500

    try to add stall

--- src/api/stall/router.py
+++ src/api/stall/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from src.api.stall.services import stall_get_all_method, stall_get_all_vid_iz, stall_get_all_god_nach
+from src.api.stall.services import stall_get_all_method, stall_get_all_vid_iz, stall_get_all_god_nach, \
+    stall_get_all_god_end, stall_get_all_tgf, stall_get_all_nom_1000, stall_get_all_org_isp, stall_get_all_scale
 
 stall_router = APIRouter(prefix="/stall", tags=["Все отчеты карты"])
 
@@ -33,3 +34,52 @@ async def get_stall_get_all_vid_iz():
 async def get_stall_get_all_god_nach():
     content = await stall_get_all_god_nach()
     return content
+
+@stall_router.get(path='/all/god_end/unique',
+                status_code=200,
+                name='Получить все уникальные "Год окончания" для всех Отчетов',
+                tags=['Все отчеты карты'],
+                description='Получить все уникальные "Год окончания" для всех Отчетов')
+async def get_stall_get_all_god_end():
+    content = await stall_get_all_god_end()
+    return content
+
+@stall_router.get(path='/all/tgf/unique',
+                status_code=200,
+                name='Получить все уникальные "ТГФ" для всех Отчетов',
+                tags=['Все отчеты карты'],
+                description='Получить все уникальные "ТГФ" для всех Отчетов')
+async def get_stall_get_all_tgf():
+    content = await stall_get_all_tgf()
+    return content
+
+
+@stall_router.get(path='/all/nom_1000/unique',
+                status_code=200,
+                name='Получить все уникальные "Лист" для всех Отчетов',
+                tags=['Все отчеты карты'],
+                description='Получить все уникальные "Лист" для всех Отчетов')
+async def get_stall_get_all_nom_1000():
+    content = await stall_get_all_nom_1000()
+    return content
+
+
+@stall_router.get(path='/all/org_isp/unique',
+                status_code=200,
+                name='Получить все уникальные "Организация" для всех Отчетов',
+                tags=['Все отчеты карты'],
+                description='Получить все уникальные "Организация" для всех Отчетов')
+async def get_stall_get_all_org_isp():
+    content = await stall_get_all_org_isp()
+    return content
+
+
+@stall_router.get(path='/all/scale/unique',
+                status_code=200,
+                name='Получить все уникальные "Масштаб" для всех Отчетов',
+                tags=['Все отчеты карты'],
+                description='Получить все уникальные "Масштаб" для всех Отчетов')
+async def get_stall_get_all_scale():
+    content = await stall_get_all_scale()
+    return content
+
--- src/api/stall/services.py
+++ src/api/stall/services.py
@@ -151,6 +151,241 @@ async def stall_get_all_god_nach():
             await session.close()
     return content
 
+async def stall_get_all_god_end():
+    content = {"msg": cfg.MSG_ERROR}
+    # log = set_logger(cfg.AREA_FILE_LOG)
+
+    try:
+        async with async_session_maker() as session:
+            res_sta = await session.scalars(
+                select(M_STA.god_end)
+                .where(M_STA.god_end.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STA.god_end)
+            )
+            _all_sta = res_sta.all()
+
+            res_stl = await session.scalars(
+                select(M_STL.god_end)
+                .where(M_STL.god_end.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STL.god_end)
+            )
+            _all_stl = res_stl.all()
+
+            res_stp = await session.scalars(
+                select(M_STP.god_end)
+                .where(M_STP.god_end.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STP.god_end)
+            )
+            _all_stp = res_stp.all()
+
+            all_stall = _all_sta + _all_stl + _all_stp
+            all_uniq = set(all_stall)
+            _all: list = sorted(all_uniq)  # sorted возвращает новый отсортированный список
+            cnt = len(_all)
+            content = {"msg": cfg.MSG_OK, "count": cnt, "data": _all}
+            # log.info("ngp load successfully")
+            return content
+    except Exception as e:
+        cont_err = f"fail. can't read table ({M_STA.__tablename__})"
+        content = {"msg": cfg.MSG_ERROR, "data": f"Exception occurred {str(e)} . {cont_err}"}
+        print(content)
+    finally:
+        if session is not None:
+            await session.close()
+    return content
+
+
+async def stall_get_all_tgf():
+    content = {"msg": cfg.MSG_ERROR}
+    # log = set_logger(cfg.AREA_FILE_LOG)
+
+    try:
+        async with async_session_maker() as session:
+            res_sta = await session.scalars(
+                select(M_STA.tgf)
+                .where(M_STA.tgf.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STA.tgf)
+            )
+            _all_sta = res_sta.all()
+
+            res_stl = await session.scalars(
+                select(M_STL.tgf)
+                .where(M_STL.tgf.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STL.tgf)
+            )
+            _all_stl = res_stl.all()
+
+            res_stp = await session.scalars(
+                select(M_STP.tgf)
+                .where(M_STP.tgf.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STP.tgf)
+            )
+            _all_stp = res_stp.all()
+
+            all_stall = _all_sta + _all_stl + _all_stp
+            all_uniq = set(all_stall)
+            _all: list = sorted(all_uniq)  # sorted возвращает новый отсортированный список
+            cnt = len(_all)
+            content = {"msg": cfg.MSG_OK, "count": cnt, "data": _all}
+            # log.info("ngp load successfully")
+            return content
+    except Exception as e:
+        cont_err = f"fail. can't read table ({M_STA.__tablename__})"
+        content = {"msg": cfg.MSG_ERROR, "data": f"Exception occurred {str(e)} . {cont_err}"}
+        print(content)
+    finally:
+        if session is not None:
+            await session.close()
+    return content
+
+
+async def stall_get_all_nom_1000():
+    content = {"msg": cfg.MSG_ERROR}
+    # log = set_logger(cfg.AREA_FILE_LOG)
+
+    try:
+        async with async_session_maker() as session:
+            res_sta = await session.scalars(
+                select(M_STA.nom_1000)
+                .where(M_STA.nom_1000.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STA.nom_1000)
+            )
+            _all_sta = res_sta.all()
+
+            res_stl = await session.scalars(
+                select(M_STL.nom_1000)
+                .where(M_STL.nom_1000.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STL.nom_1000)
+            )
+            _all_stl = res_stl.all()
+
+            res_stp = await session.scalars(
+                select(M_STP.nom_1000)
+                .where(M_STP.nom_1000.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STP.nom_1000)
+            )
+            _all_stp = res_stp.all()
+
+            all_stall = _all_sta + _all_stl + _all_stp
+            all_uniq = set(all_stall)
+            _all: list = sorted(all_uniq)  # sorted возвращает новый отсортированный список
+            cnt = len(_all)
+            content = {"msg": cfg.MSG_OK, "count": cnt, "data": _all}
+            # log.info("ngp load successfully")
+            return content
+    except Exception as e:
+        cont_err = f"fail. can't read table ({M_STA.__tablename__})"
+        content = {"msg": cfg.MSG_ERROR, "data": f"Exception occurred {str(e)} . {cont_err}"}
+        print(content)
+    finally:
+        if session is not None:
+            await session.close()
+    return content
+
+
+async def stall_get_all_org_isp():
+    content = {"msg": cfg.MSG_ERROR}
+    # log = set_logger(cfg.AREA_FILE_LOG)
+
+    try:
+        async with async_session_maker() as session:
+            res_sta = await session.scalars(
+                select(M_STA.org_isp)
+                .where(M_STA.org_isp.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STA.org_isp)
+            )
+            _all_sta = res_sta.all()
+
+            res_stl = await session.scalars(
+                select(M_STL.org_isp)
+                .where(M_STL.org_isp.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STL.org_isp)
+            )
+            _all_stl = res_stl.all()
+
+            res_stp = await session.scalars(
+                select(M_STP.org_isp)
+                .where(M_STP.org_isp.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STP.org_isp)
+            )
+            _all_stp = res_stp.all()
+
+            all_stall = _all_sta + _all_stl + _all_stp
+            all_uniq = set(all_stall)
+            _all: list = sorted(all_uniq)  # sorted возвращает новый отсортированный список
+            cnt = len(_all)
+            content = {"msg": cfg.MSG_OK, "count": cnt, "data": _all}
+            # log.info("ngp load successfully")
+            return content
+    except Exception as e:
+        cont_err = f"fail. can't read table ({M_STA.__tablename__})"
+        content = {"msg": cfg.MSG_ERROR, "data": f"Exception occurred {str(e)} . {cont_err}"}
+        print(content)
+    finally:
+        if session is not None:
+            await session.close()
+    return content
+
+
+async def stall_get_all_scale():
+    content = {"msg": cfg.MSG_ERROR}
+    # log = set_logger(cfg.AREA_FILE_LOG)
+
+    try:
+        async with async_session_maker() as session:
+            res_sta = await session.scalars(
+                select(M_STA.scale)
+                .where(M_STA.scale.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STA.scale)
+            )
+            _all_sta = res_sta.all()
+
+            res_stl = await session.scalars(
+                select(M_STL.scale)
+                .where(M_STL.scale.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STL.scale)
+            )
+            _all_stl = res_stl.all()
+
+            res_stp = await session.scalars(
+                select(M_STP.scale)
+                .where(M_STP.scale.is_not(None))  # Фильтруем NOT NULL
+                .distinct()  # Добавляем distinct для получения уникальных значений
+                .order_by(M_STP.scale)
+            )
+            _all_stp = res_stp.all()
+
+            all_stall = _all_sta + _all_stl + _all_stp
+            all_uniq = set(all_stall)
+            _all: list = sorted(all_uniq)  # sorted возвращает новый отсортированный список
+            cnt = len(_all)
+            content = {"msg": cfg.MSG_OK, "count": cnt, "data": _all}
+            # log.info("ngp load successfully")
+            return content
+    except Exception as e:
+        cont_err = f"fail. can't read table ({M_STA.__tablename__})"
+        content = {"msg": cfg.MSG_ERROR, "data": f"Exception occurred {str(e)} . {cont_err}"}
+        print(content)
+    finally:
+        if session is not None:
+            await session.close()
+    return content
+
+
 
 
 
