bccf5ff60e843f805640ba348077e0172e8d18ef
Glory <zamaraev@gmail.com>
2026-05-05 00:53:18 +0500

    change main.py

--- src/main.py
+++ src/main.py
@@ -1,29 +1,65 @@
 # uvicorn main:app --reload
 # https://habr.com/ru/articles/705752/
+# from src.db.db import get_async_session, engine
+# import cfg
 import os
 import sys
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-
+from src import cfg
+from src import routers
 
 
 sys.path.insert(1, 'src')
 os.environ['PYTHONPATH'] = os.environ.get('PYTHONPATH', '') + ';' + os.getcwd()
 
-# import cfg
-from src import cfg
-from src import routers
 
-# from src.db.db import get_async_session, engine
+app = FastAPI(title="GDX2 API",
+    description="API для загрузки геологических отчетов",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc")
 
 
-app = FastAPI(title="GDX2 App")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Root API
+@app.get("/", status_code=200,
+         name='Get Info',
+         tags=['Главная'],
+         description='Получает информацию о сервисе')
+def root() -> JSONResponse:
+    url_swagger = f"http://{cfg.SERVER_HOST}:{cfg.SERVER_PORT}/docs"
+
+    return JSONResponse(status_code=200,
+                        content={
+                            "msg": "Success",
+                            "Info": "Hello it is FastAPI-NSI project",
+                            "Swagger Documentation": url_swagger})
+
+
+app.include_router(routers.api_router)
+
+
+if __name__ == "__main__":
+    # run()
+    # main.py - только приложение
+    uvicorn.run("main:app", host=cfg.SERVER_HOST, port=int(cfg.SERVER_PORT), reload=True)
+
+    # uvicorn.run("main:app", host=cfg.SERVER_HOST, port=int(cfg.SERVER_PORT), reload=True, workers=4)
+
+
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 # , root_path=API_VERSION
 # origins = [
 #     "http://localhost",
@@ -59,37 +95,8 @@ app = FastAPI(title="GDX2 App")
 #     allow_headers=["*"],
 # )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins = ["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-# Root API
-@app.get("/", status_code=200,
-         name='Get Info',
-         tags=['Главная'],
-         description='Получает информацию о сервисе')
-def root() -> JSONResponse:
-    url_swagger = f"http://{cfg.SERVER_HOST}:{cfg.SERVER_PORT}/docs"
-
-    return JSONResponse(status_code=200,
-                        content={
-                            "msg": "Success",
-                            "Info": "Hello it is FastAPI-NSI project",
-                            "Swagger Documentation": url_swagger})
-
-
-app.include_router(routers.api_router)
-
-
 # for router in all_routers:
 #     app.include_router(router)
-
-
 # @app.on_event("startup")
 # async def startup() -> None:
 #     pass
@@ -109,39 +116,33 @@ app.include_router(routers.api_router)
 #     if database_.is_connected:
 #         await database_.disconnect()
 
-class Server():
-    def __init__(self, as_service=False):
-        self._srv = None
-        self._svc = None
-        self.as_service = as_service
-
-    def start(self):
-
-        # Здесь можно настроить параметры хоста, порта, логирования
-        config = uvicorn.Config(app)
-        config.host = cfg.SERVER_HOST
-        config.port = int(cfg.SERVER_PORT)
-        config.reload = True
-        config.workers = 4
-
-        self._srv = uvicorn.Server(config=config)
-
-        if self.as_service:
-            self._srv.install_signal_handlers = lambda: None
-
-        self._srv.run()
-
-    def stop(self):
-        if self._srv is not None:
-            self._srv.should_exit = True
-
-
-def run():
-    srv = Server()
-    srv.start()
-
-
-if __name__ == "__main__":
-    run()
-    # set_logger()
-    # uvicorn.run("main:app", host=cfg.SERVER_HOST, port=int(cfg.SERVER_PORT), reload=True, workers=4)
+# class Server():
+#     def __init__(self, as_service=False):
+#         self._srv = None
+#         self._svc = None
+#         self.as_service = as_service
+#
+#     def start(self):
+#
+#         # Здесь можно настроить параметры хоста, порта, логирования
+#         config = uvicorn.Config(app)
+#         config.host = cfg.SERVER_HOST
+#         config.port = int(cfg.SERVER_PORT)
+#         config.reload = True
+#         config.workers = 4
+#
+#         self._srv = uvicorn.Server(config=config)
+#
+#         if self.as_service:
+#             self._srv.install_signal_handlers = lambda: None
+#
+#         self._srv.run()
+#
+#     def stop(self):
+#         if self._srv is not None:
+#             self._srv.should_exit = True
+#
+#
+# def run():
+#     srv = Server()
+#     srv.start()
